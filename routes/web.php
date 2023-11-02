b8967d619d35c36456504dc2b4ef06ec03288f9e
Konyuka <michaelsaiba84@gmail.com>
2023-11-02 14:49:48 +0300

    https redirect

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,10 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
